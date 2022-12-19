6fca7888d4853e5ba08d36fce9051fc285b3ea58
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2022-12-19 17:46:00 -0600

    Sending Topics in Mail

--- js/script.js
+++ js/script.js
@@ -2,38 +2,48 @@ generate();
 
 const form = document.forms["enviar"];
 
-const topico = "Contacto";
+const topico = "";
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    let input_captcha = document.getElementById("captcha").value;
-    let captcha = document.getElementById("image").innerHTML;
-    const msg = document.getElementById("msg");
-    if (input_captcha == captcha) {
-        const mensaje = form["mensaje"].value;
-        const nombre = form["nombre"].value;
-        const destino = form["correo_destino"].value;
-        Email.send({
-            SecureToken : "19e6f42c-2c74-423e-83ef-2ac93be2c6b6",
-            To: [destino, "contactopaoloinspires@gmail.com"],
-            From: "contactopaoloinspires@gmail.com",
-            Subject: "Propuesta de " + nombre + " - " + topico,
-            Body: "<h4>Este es un correo automático que se le ha enviado tanto a Paolo como a usted.</h4><h4>Paolo pronto lo contactará con una respuesta</h4> <strong>Mensaje: </strong>" + mensaje,
-        }).then(function () {
-            msg.style.color = "#61b752";
-            msg.innerHTML = "Mensaje Enviado con Éxito";
+    if (topico !== "") {
+        let input_captcha = document.getElementById("captcha").value;
+        let captcha = document.getElementById("image").innerHTML;
+        const msg = document.getElementById("msg");
+        if (input_captcha == captcha) {
+            const mensaje = form["mensaje"].value;
+            const nombre = form["nombre"].value;
+            const destino = form["correo_destino"].value;
+            Email.send({
+                SecureToken : "19e6f42c-2c74-423e-83ef-2ac93be2c6b6",
+                To: [destino, "contactopaoloinspires@gmail.com"],
+                From: "contactopaoloinspires@gmail.com",
+                Subject: "Propuesta de " + nombre + " - " + topico,
+                Body: "<h4>Este es un correo automático que se le ha enviado tanto a Paolo como a usted.</h4><h4>Paolo pronto lo contactará con una respuesta</h4> <strong>Mensaje: </strong>" + mensaje,
+            }).then(function () {
+                msg.style.color = "#61b752";
+                msg.innerHTML = "Mensaje Enviado con Éxito";
+                setTimeout(function(){
+                    msg.innerHTML = "";
+                }, 5000);
+                form.reset();
+                generate();
+            });
+        } else {
+            msg.style.color = "#ff004f";
+            msg.innerHTML = "CAPTCHA Incorrecto";
             setTimeout(function(){
                 msg.innerHTML = "";
             }, 5000);
-            form.reset();
-            generate();
-        });
+        }
     } else {
-        msg.style.color = "#ff004f";
-        msg.innerHTML = "CAPTCHA Incorrecto";
-        setTimeout(function(){
-            msg.innerHTML = "";
-        }, 5000);
+        msg.style.color = "#ffff00";
+        msg.innerHTML = "Espera a que la página cargue por completo";
+        document.onreadystatechange = function () {
+            if (document.readyState == "complete") {
+                msg.innerHTML = "";
+            }
+        }
     }
 })
 
