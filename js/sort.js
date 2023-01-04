7397f86bc8a52db14e9a0bea05716255ab30f151
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2023-01-03 23:12:50 -0600

    select for sorting element

---
+++ js/sort.js
@@ -0,0 +1,13 @@
+function sort() {
+   
+}
+
+extend()
+
+function extend() {
+    if (document.getElementById("select-container").style.margin[8] === "-") {
+        document.getElementById("select-container").style.margin = "0 0 45px 0";
+    } else  {
+        document.getElementById("select-container").style.margin = "0 0 -45px 0";
+    }
+}
