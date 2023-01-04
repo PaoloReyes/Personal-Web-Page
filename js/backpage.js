25e441051c3fa050dd9e43e1fcd26d77adbf17d3
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2023-01-03 18:50:30 -0600

    intelligent backing

--- js/backpage.js
+++ js/backpage.js
@@ -1,7 +1,11 @@
 let header = document.getElementsByClassName("header-subpage")[0];
 
+tempcontent = header.innerHTML;
+
 if (document.referrer == "https://paoloreyes.github.io/web/proyectos") {
     header.innerHTML = '<a href="https://paoloreyes.github.io/web/proyectos/"><i class="fa-solid fa-angles-left"></i></a>'
 } else {
     header.innerHTML = '<a href="https://paoloreyes.github.io/web/"><i class="fa-solid fa-angles-left"></i></a>'
 }
+
+header.innerHTML += tempcontent;
